2db696b8749d9476b4eeaf97126ef5a5f231b151
Core4Web <Core.4Web@gmail.com>
2020-10-15 09:29:54 +0300

    Added Entity Framework packages and Command context

---
+++ src/CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,15 @@
+﻿using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Controllers.Data
+{
+    public class CommandContext: DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options): base(options)
+        {
+            
+        }
+        
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
