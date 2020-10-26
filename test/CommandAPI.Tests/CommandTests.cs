693df1c4f334ff89a4ea4ff082bdd9a2f834de46
Core4Web <Core.4Web@gmail.com>
2020-10-26 15:35:48 +0300

    Add First Test (CommandController)

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CommandAPI.Models;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests: IDisposable
+    {
+        private Command testCommand;
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform="xUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand=null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Act
+            testCommand.Platform = "xUnit";
+            //Assert
+            Assert.Equal("xUnit", testCommand.Platform);
+        }
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Act
+            testCommand.CommandLine = "dotnet test";
+            //Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+        }
+    }
+}
